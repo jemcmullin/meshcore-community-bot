c5f10dc1681296bb4d833a53995d54d34b6d68a0
jemcmullin <jemcmullin@gmail.com>
2026-03-06 05:52:18 -0700

    Remove embedded meshcore-bot (converting to submodule)

--- meshcore-bot/modules/commands/alternatives/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""
-Alternative plugins directory
-Place alternative implementations of commands here to override default plugins.
-See README.md in this directory for usage instructions.
-"""
-
--- meshcore-bot/modules/commands/alternatives/inactive/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""
-Inactive alternative plugins directory
-Place alternative plugin implementations here that you want to keep but not load.
-Plugins in this directory will be ignored by the plugin loader.
-"""
-
